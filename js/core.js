96d4b9d7a90b5b56f50f9c3d8581c6fe6f963411
liquidz <liquidz.uo@gmail.com>
2010-09-15 00:17:22 +0900

    aa

--- js/core.js
+++ js/core.js
@@ -11,12 +11,19 @@ MyApp.init = function(){
 		response: os.data(os.owner, "tshirt_name", "tshirt_number", "tshirt_color", "tshirt_word_color", "tshirt_name_size", "tshirt_number_size")
 	}, function(res){
 		var data = res.response[res.owner.getId()];
-		var name = (data.tshirt_name === undefined) ? res.owner.getDisplayName() : data.tshirt_name;
-		var no = (data.tshirt_number === undefined) ? "X" : data.tshirt_number;
-		var color = (data.tshirt_color === undefined) ? "#04c" : data.tshirt_color;
-		var word_color = (data.tshirt_word_color === undefined) ? "#fff" : data.tshirt_word_color;
-		var name_size = (data.tshirt_name_size === undefined) ? "17px" : data.tshirt_name_size;
-		var number_size = (data.tshirt_number_size === undefined) ? "60px" : data.tshirt_number_size;
+		//var name = (data.tshirt_name === undefined) ? res.owner.getDisplayName() : data.tshirt_name;
+		//var no = (data.tshirt_number === undefined) ? "X" : data.tshirt_number;
+		//var color = (data.tshirt_color === undefined) ? "#04c" : data.tshirt_color;
+		//var word_color = (data.tshirt_word_color === undefined) ? "#fff" : data.tshirt_word_color;
+		//var name_size = (data.tshirt_name_size === undefined) ? "17px" : data.tshirt_name_size;
+		//var number_size = (data.tshirt_number_size === undefined) ? "60px" : data.tshirt_number_size;
+
+		var name = (data === undefined) ? res.owner.getDisplayName() : data.tshirt_name;
+		var no = (data === undefined) ? "X" : data.tshirt_number;
+		var color = (data === undefined) ? "#04c" : data.tshirt_color;
+		var word_color = (data === undefined) ? "#fff" : data.tshirt_word_color;
+		var name_size = (data === undefined) ? "17px" : data.tshirt_name_size;
+		var number_size = (data === undefined) ? "60px" : data.tshirt_number_size;
 
 		$("#new_name").val(name);
 		$("#new_number").val(no);
