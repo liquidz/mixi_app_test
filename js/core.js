d71ad720bb26e3016aae346bd4c1d18d45113192
liquidz <liquidz.uo@gmail.com>
2010-09-14 21:24:11 +0900

    aa

--- js/core.js
+++ js/core.js
@@ -14,10 +14,16 @@ MyOpenSocial.idSpec = function(groupId, userId){
 	params[opensocial.IdSpec.Field.GROUP_ID] = groupId;
 	return opensocial.newIdSpec(params);
 };
+MyOpenSocial.toList = function(obj){
+	var res = [];
+	for(var i = 0, l = obj.length; i < l; ++i){ res.push(obj[i]); }
+	return res;
+};
 MyOpenSocial.data = function(){
 	if(arguments.length < 2){ return null; }
-	var idspec = arguments[0];
-	var kes = arguments.slice(1);
+	var alist = this.toList(arguments);
+	var idspec = alist[0];
+	var kes = alist.slice(1);
 	return function(req){
 		return req.newFetchPersonAppDataRequest(idspec, kes);
 	};
@@ -48,7 +54,7 @@ MyOpenSocial.get = function(){
 				//return request.add(val(req), key);
 				return val(req);
 			}
-		}].concat(arguments)
+		}].concat(this.toList(arguments))
 	);
 };
 
@@ -78,7 +84,7 @@ MyOpenSocial.set = function(){
 	this.sendRequest.apply(this,
 		[function(req, key, val){
 			return req.newUpdatePersonAppDataRequest(this.viewer, key, val);
-		}].concat(arguments)
+		}].concat(this.toList(arguments))
 	);
 };
 //MyOpenSocial.set = function(mapping, callback){
