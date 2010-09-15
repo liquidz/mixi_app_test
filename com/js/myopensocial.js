5c2df03fbc2f925858c8e86aead17dc7bf1b7546
liquidz <liquidz.uo@gmail.com>
2010-09-15 19:18:41 +0900

    add

---
+++ com/js/myopensocial.js
@@ -0,0 +1,56 @@
+MyOpenSocial = {};
+MyOpenSocial.viewer = opensocial.IdSpec.PersonId.VIEWER;
+MyOpenSocial.owner = opensocial.IdSpec.PersonId.OWNER;
+MyOpenSocial.idSpec = function(groupId, userId){
+	var params = {};
+	params[opensocial.IdSpec.Field.USER_ID] = (userId === undefined) ? this.viewer : userId;
+	params[opensocial.IdSpec.Field.GROUP_ID] = groupId;
+	return opensocial.newIdSpec(params);
+};
+MyOpenSocial.toList = function(obj){
+	var res = [];
+	for(var i = 0, l = obj.length; i < l; ++i){ res.push(obj[i]); }
+	return res;
+};
+MyOpenSocial.data = function(){
+	if(arguments.length < 2){ return null; }
+	var alist = this.toList(arguments);
+	var idspec = alist[0];
+	var kes = alist.slice(1);
+	return function(req){
+		return req.newFetchPersonAppDataRequest(idspec, kes);
+	};
+};
+
+MyOpenSocial.sendRequest = function(conv, mapping, callback){
+	var request = opensocial.newDataRequest();
+	for(var key in mapping){
+		request.add(conv(request, key, mapping[key]), key);
+	}
+	return request.send(function(data){
+		if($.isFunction(callback)){
+			var res = {};
+			for(var key in mapping){ res[key] = data.get(key).getData(); }
+			callback(res);
+		}
+	});
+};
+
+MyOpenSocial.get = function(mapping, callback){
+	var self = this;
+	this.sendRequest(function(req, key, val){
+		if(val === self.viewer || val === self.owner){
+			return req.newFetchPersonRequest(val);
+		} else if($.isFunction(val)){
+			return val(req);
+		}
+	}, mapping, callback);
+};
+
+MyOpenSocial.set = function(mapping, callback){
+	var self = this;
+	this.sendRequest(function(req, key, val){
+		return req.newUpdatePersonAppDataRequest(self.viewer, key, val);
+	}, mapping, callback);
+};
+
--- tshirt/js/core.js
+++ tshirt/js/core.js
@@ -1,6 +1,114 @@
 var MyApp = {};
 
-MyApp.update = function(name, no){
+MyApp.defaultData = {
+	tshirt_number: "X",
+	tshirt_color: "#04c",
+	tshirt_word_color: "#fff",
+	tshirt_name_size: "17px",
+	tshirt_number_size: "60px"//,
+//	tshirt_font: "Droid Sans"
+};
+
+MyApp.loadFont = function(fontName){
+	WebFontConfig = {
+		google: { families: [fontName]}
+	};
+	var wf = document.createElement("script");
+	wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+		'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+	wf.type = 'text/javascript';
+	wf.async = 'true';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(wf, s);
+};
+
+MyApp.get = function(callback){
+	var os = MyOpenSocial;
+	var withDefault = function(obj, key, defaultVal){
+		return((obj === undefined) ? defaultVal : obj[key]);
+	};
+
+	os.get({
+		viewer: os.viewer,
+		owner: os.owner,
+		response: os.data(os.owner, "tshirt_name", "tshirt_number", "tshirt_color", "tshirt_word_color", "tshirt_name_size", "tshirt_number_size")
+	}, function(res){
+		var data = res.response[res.owner.getId()];
+		var getData = {};
+		for(var k in MyApp.defaultData){
+			getData[k] = withDefault(data, k, MyApp.defaultData[k])
+		}
+		getData.name = withDefault(data, "tshirt_name", res.owner.getDisplayName());
+		getData.viewer = res.viewer;
+		getData.owner = res.owner;
+
+		callback(getData);
+	});
+};
+
+MyApp.set = function(data){
+	//this.loadFont(data.tshirt_font);
+	//
+	DD_belatedPNG.fix(".iepngfix");
+	$("#tshirt").css("background-color", data.tshirt_color);
+	$("#tshirt p").css("color", data.tshirt_word_color);
+	//$(".wf-inactive #tshirt p").css("font-family", "sans-serif");
+	//$(".wf-active #tshirt p").css("font-family", data.tshirt_font);
+	$("#tshirt p.name").html(data.tshirt_name).css("font-size", data.tshirt_name_size);
+	$("#tshirt p.number").html(data.tshirt_number).css("font-size", data.tshirt_number_size);
+};
+
+MyApp.bindEvents = function(){
+	$("#save_setting").bind("click", MyApp.saveSetting);
+	$(".color").each(function(){
+		var self = this;
+		$(this).ColorPicker({
+			color: $(this).val(),
+			onShow: function(cp){ $(cp).fadeIn(250); },
+			onHide: function(cp){ $(cp).fadeOut(250); },
+			onChange: function(hsb, hex, rgb){
+				$(self).val("#" + hex);
+				if(self.id === "tshirt_color"){
+					$("#tshirt").css("background-color", "#" + hex);
+				} else {
+					$("#tshirt p").css("color", "#" + hex);
+				}
+			}
+		});
+	});
+};
+
+MyApp.init_new = function(){
+	var self = this;
+	this.get(function(res){
+		self.set(res);
+		gadgets.window.adjustHeight();
+
+		if(res.viewer.getId() === res.owner.getId()){
+			for(var k in res){
+				var e = $("#new_" + k);
+				if(e.length === 0){ continue; }
+				e.val(res[k]);
+			};
+
+			self.bindEvents();
+
+		} else {
+			$("#change_form").hide();
+		}
+	});
+};
+
+MyApp.saveSetting = function(){
+	var newData = {};
+	for(var key in this.defaultData){
+		newData[key] = $("#new_" + key).val();
+		if(newData[key] === ""){ return false; }
+	}
+
+	MyOpenSocial.set(newData, function(){
+		MyApp.set(newData);
+	});
 };
 
 MyApp.init = function(){
@@ -58,6 +166,23 @@ MyApp.init = function(){
 						$("#tshirt p.number").html(new_number).css("font-size", new_number_size);
 					});
 				}
+			});
+
+			$(".color").each(function(){
+				var self = this;
+				$(this).ColorPicker({
+					color: $(this).val(),
+					onShow: function(cp){ $(cp).fadeIn(250); },
+					onHide: function(cp){ $(cp).fadeOut(250); },
+					onChange: function(hsb, hex, rgb){
+						$(self).val("#" + hex);
+						if(self.id === "tshirt_color"){
+							$("#tshirt").css("background-color", "#" + hex);
+						} else {
+							$("#tshirt p").css("color", "#" + hex);
+						}
+					}
+				});
 			});
 		} else {
 			$("#change_form").hide();
