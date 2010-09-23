9babedec5f33d4a6b38090250f9e47f720411848
liquidz <liquidz.uo@gmail.com>
2010-09-23 18:00:48 +0900

    update

--- tshirt/js/common.js
+++ tshirt/js/common.js
@@ -120,7 +120,11 @@ TShirt.resetSetting = function(){
 				var e = $("#new_" + k);
 				if(e.length === 0){ continue; }
 				//e.val(data[k].replace(/\s*px\s*/, ""));
-				e.val(this.unit(data[k]));
+				if(k.indexOf("size") !== -1 || k.indexOf("top") !== -1 || k.indexOf("left") !== -1){
+					e.val(this.unit(data[k]));
+				} else {
+					e.val(data[k]);
+				}
 			};
 			this.set(data);
 		}));
